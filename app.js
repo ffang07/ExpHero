ac4a7d6b814b7cfb7ea46d4e6cabfb11afe9fc68
ffang07 <ffang07@foxmail.com>
2020-04-18 21:16:08 +0800

    建Express服务器,路由,托管静态资源

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+let router = require('./router');
+
+const app = express();
+app.set('view engine','ejs');
+app.use('/node_modules',express.static('node_modules'),)
+
+app.listen(3000,()=>{
+    console.log('server is runnig at http:127.0.0.1:3000');
+})
+
+app.use(router);
